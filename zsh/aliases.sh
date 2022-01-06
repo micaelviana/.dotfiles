b50feae347d3610fc92219b0a39de1500004c7bc
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2022-01-06 18:56:28 -0400

    zsh - changing aliases - lsd

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -5,9 +5,9 @@ alias py=python3
 alias open=xdg-open
 #lsd aliases
 alias ls='lsd'
-alias l='ls -l'
-alias la='ls -a'
-alias lla='ls -la'
+alias l='lsd -l'
+alias la='lsd -a'
+alias lla='lsd -la'
 
 alias si="__zoxide_zi"
 
