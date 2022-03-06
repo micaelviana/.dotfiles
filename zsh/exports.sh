7f5be4bec78cf1b62ffce3d089b607cb3667b2df
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2022-03-06 09:52:06 -0400

    remove bloat config - nodejs path

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -3,7 +3,6 @@ export EDITOR=nvim
 export VISUAL=code
 export SUDO_EDITOR=nvim
 export mount="$HOME/Documents/Mountpoint"
-#export PATH=~/.npm-global/bin:$PATH
 
 if [ -d "$HOME/.local/bin" ] ;
   then PATH="$HOME/.local/bin:$PATH"
