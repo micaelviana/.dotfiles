de7b08aadf49b00c54ddf679a2578170c9512d8f
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2022-03-06 09:50:19 -0400

    update zshrc

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -3,7 +3,7 @@ export EDITOR=nvim
 export VISUAL=code
 export SUDO_EDITOR=nvim
 export mount="$HOME/Documents/Mountpoint"
-export PATH=~/.npm-global/bin:$PATH
+#export PATH=~/.npm-global/bin:$PATH
 
 if [ -d "$HOME/.local/bin" ] ;
   then PATH="$HOME/.local/bin:$PATH"
