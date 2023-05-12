fc42baaf4174501b042bc7716039cc806d150700
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2023-05-12 15:23:08 -0400

    zsh - add some functions and some aliases

--- zsh/funcs.sh
+++ zsh/funcs.sh
@@ -15,6 +15,10 @@ function tao () {
   [ -z "$TMUX"  ] && { tmux attach || tmux new-session ;}
 }
 
+function gc { 
+    git commit -m "$@"; 
+}
+
 function add_oh_my_zsh_plugin() {
     # check if the URL is provided
     if [ -z "$1" ]; then
@@ -28,10 +32,12 @@ function add_oh_my_zsh_plugin() {
     # add the plugin to the list of plugins in the .zshrc file
     url="$1"
     repo=$(basename "$url" .git)
-    sed -i "s/\(plugins=(.*\))/\1 $repo)/" ~/.config/zsh/.zshrc
-    
-    # reload the .zshrc file
-    source "$HOME/.config/zsh/.zshrc"
+
+    if [ -f ~/.zshrc ]; then
+        sed -i "s/\(plugins=(.*\))/\1 $repo)/" ~/.zshrc
+    else
+        sed -i "s/\(plugins=(.*\))/\1 $repo)/" ~/.config/zsh/.zshrc
+    fi
     
     echo "Plugin added: $(basename "$1" .git)"
 }
