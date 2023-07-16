3eff5b13c3e7f7d3b35af7b211ef2e4ae02a6b9f
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2023-07-16 16:26:42 -0400

    add some functions

--- zsh/.config/zsh/functions.sh
+++ zsh/.config/zsh/functions.sh
@@ -20,3 +20,19 @@ function drm() {
 
   [ -n "$cid" ] && docker rm "$cid"
 }
+
+#show all paths
+function path(){
+    echo -e ${PATH//:/\\n}
+}
+
+#start tmux in the same section
+function tao(){
+    [ -z "$TMUX"  ] && { tmux attach || tmux new-session ;}
+}
+
+#open files in neovim using fzf picker
+function vif(){
+    fd --type f --hidden --exclude .git --exclude .vscode --exclude node_modules --print0 | fzf --layout=reverse --height=50% --read0 --print0 --exit-0 | xargs -r -0 nvim
+}
+
