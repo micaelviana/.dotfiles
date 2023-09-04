678909a69f6a25c5d99c75fb31b9d9c277dea5cf
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2023-09-03 22:25:13 -0400

    zsh - add new functions to add paths

--- zsh/.config/zsh/exports.sh
+++ zsh/.config/zsh/exports.sh
@@ -4,6 +4,21 @@ eval "$(zoxide init zsh)"
 eval "$(fnm env --use-on-cd)"
 export EDITOR=nvim
 export SUDO_EDITOR=nvim
-export PATH="$HOME/.local/bin:$PATH"
+# export PATH="$HOME/.local/bin:$PATH"
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export MANROFFOPT="-c"
+
+
+#from ThePrimeagen: add a folder to PATH
+addToPath() {
+    if [[ "$PATH" != *"$1"* ]]; then
+        export PATH=$PATH:$1
+    fi
+}
+addToPathFront() {
+    if [[ "$PATH" != *"$1"* ]]; then
+        export PATH=$1:$PATH
+    fi
+}
+addToPathFront "$HOME"/.local/bin
+addToPathFront /mnt/c/Users/Micael/AppData/Local/shims
