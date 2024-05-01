5c7492213cac429dfb0853b775090e4872154a0c
Micael Viana <44692237+micaelviana@users.noreply.github.com>
2024-05-01 08:06:30 -0700

    Update install-yay.sh

--- scripts/install-yay.sh
+++ scripts/install-yay.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-cd tmp
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
+
+if [ -f "/etc/arch-release" ]; then
+    cd tmp
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si
+fi
