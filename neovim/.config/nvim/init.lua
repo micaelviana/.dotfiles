161d857d625eaa8eca62255674034de2470eb380
Micael Viana <micael.vianasilva@gmail.com>
2024-11-13 18:29:47 -0400

    nvim, update clipboard config, win32yank -

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -34,21 +34,22 @@ vim.schedule(function()
   opt.clipboard = 'unnamedplus'
 end)
 
-if vim.fn.has('wsl') == 1 then
-    vim.g.clipboard = {
-        name = 'win32yank-wsl',
-        copy = {
-            ['+'] =  'win32yank.exe -i --crlf',
-            ['*'] =  'win32yank.exe -i --crlf',
-        },
-        paste = {
-            ['+'] = 'win32yank.exe -o --lf',
-            ['*'] = 'win32yank.exe -o --lf',
-        },
-        cache_enabled = true,
-    }
-end
-
+--no longer need, install wl-clipboard
+-- if vim.fn.has('wsl') == 1 then
+--     vim.g.clipboard = {
+--         name = 'win32yank-wsl',
+--         copy = {
+--             ['+'] =  'win32yank.exe -i --crlf',
+--             ['*'] =  'win32yank.exe -i --crlf',
+--         },
+--         paste = {
+--             ['+'] = 'win32yank.exe -o --lf',
+--             ['*'] = 'win32yank.exe -o --lf',
+--         },
+--         cache_enabled = true,
+--     }
+-- end
+--
 -- COMMANDS
 vim.api.nvim_create_user_command('WindowsCleaning','%s/\r/' , {})
 
