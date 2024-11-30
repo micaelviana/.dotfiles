87665c22747e62385551e2c4d55b140b1b2667ce
Micael Viana <micael.vianasilva@gmail.com>
2024-11-30 13:52:16 -0400

    nvim, from everforest to kanagawa

--- neovim/.config/nvim/lua/plugins/colors.lua
+++ neovim/.config/nvim/lua/plugins/colors.lua
@@ -3,17 +3,21 @@ return{
     { "EdenEast/nightfox.nvim",     lazy = true }, -- lazy
     { "bluz71/vim-nightfly-colors", name = "nightfly",   lazy = true },
     { 'sainnhe/sonokai',            lazy = true },
-    { "rebelot/kanagawa.nvim",      lazy = true },
     { "rose-pine/neovim",           name = "rose-pine",  lazy = true },
     {
         'sainnhe/everforest',
-        lazy = false,
-        priority = 1000,
-        config = function()
+        lazy = true,
+        -- config = function()
             -- Optionally configure and load the colorscheme
             -- directly inside the plugin declaration.
-            vim.g.everforest_enable_italic = true
-            vim.cmd.colorscheme('everforest')
-        end
+            -- vim.g.everforest_enable_italic = true
+        -- end
+    },
+    { "rebelot/kanagawa.nvim",
+    lazy = false ,
+    priority = 1000,
+    config = function ()
+        vim.cmd.colorscheme('kanagawa')
+    end
     },
 }
