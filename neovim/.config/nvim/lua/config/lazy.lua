5effa0b6c701d71caa972ecc8952054e70d8dc67
Micael Viana <micael.vianasilva@gmail.com>
2024-12-06 23:27:58 -0400

    nvim, change theme to kanagawa

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -33,7 +33,7 @@ require("lazy").setup({
     }, --end spec
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "nightfly" } },
+    install = { colorscheme = { "kanagawa" } },
     -- automatically check for plugin updates
 })
 
--- neovim/.config/nvim/lua/plugins/colors.lua
+++ neovim/.config/nvim/lua/plugins/colors.lua
@@ -1,19 +1,9 @@
 return{
-    -- define a theme: {user/repo, lazy=false, priority=1000, vim.cmd.colorscheme('name')}
+    -- define a theme: {user/repo, lazy=false, priority=1000, config=function()vim.cmd.colorscheme('name')end}
     { "EdenEast/nightfox.nvim",     lazy = true }, -- lazy
     { "bluz71/vim-nightfly-colors", name = "nightfly",   lazy = true },
     { 'sainnhe/sonokai',            lazy = true },
     { "rose-pine/neovim",           name = "rose-pine",  lazy = true },
-    {
-        'sainnhe/everforest',lazy = true,
-    },
-    { "rebelot/kanagawa.nvim", lazy = true ,
-    },
-{ "bluz71/vim-nightfly-colors",
-    lazy = false ,
-    priority = 1000,
-    config = function ()
-        vim.cmd.colorscheme('nightfly')
-    end
-    }
+    {'sainnhe/everforest',lazy = true,},
+    { "rebelot/kanagawa.nvim", lazy = false , priority=1000, config=function()vim.cmd.colorscheme('kanagawa') end},
 }
