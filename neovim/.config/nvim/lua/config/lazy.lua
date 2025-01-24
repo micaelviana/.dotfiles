cbcf5aa2d14eaf5430a130eec5c12f40fbd45b3f
Micael Viana <micael.vianasilva@gmail.com>
2025-01-24 15:01:30 -0400

    nvim, set everforest on lazyvim

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -33,7 +33,7 @@ require("lazy").setup({
     }, --end spec
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "kanagawa" } },
+    install = { colorscheme = { "everforest" } },
     -- automatically check for plugin updates
 })
 
